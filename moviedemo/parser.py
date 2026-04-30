138dc24c8b2273940097127ee4611deb51c065c2
kibuouo <glitterkibo@gmail.com>
2026-04-30 22:40:03 +0800

    无提示写

--- moviedemo/parser.py
+++
@@ -1,5 +0,0 @@
-from bs4 import BeautifulSoup
-def parser_html(html):
-    soup=BeautifulSoup(html,"html.parser")
-    str=soup.find_all("a",class_="DyCardBottom-cardTitle")
-    return str
--- moviedemo/spider.py
+++
@@ -1,7 +0,0 @@
-import requests
-def getUrl(url):
-    header={
-        "User-Agent":"Mozilla/5.0"
-    }
-    responser = requests.get(url,headers=header)
-    return responser.text
